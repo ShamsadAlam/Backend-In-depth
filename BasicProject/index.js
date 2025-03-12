b187c9366ebb599b0876b2a8d49418a9bad229f7
ShamsadAlam <nobleperson7571@gmail.com>
2025-03-12 19:47:22 +0530

    get requests

--- BasicProject/index.js
+++ BasicProject/index.js
@@ -1,1 +1,20 @@
-console.log("Shamsad Alam");
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("ShamsadAlam7084");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login yourself</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
