d3d88d363e6cc6c0d23f0d63816716f4cb94899b
ShamsadAlam <nobleperson7571@gmail.com>
2025-03-15 20:44:02 +0530

    user controller and routes defined

--- 4-professional-Setup/src/utils/asyncHandler.js
+++ 4-professional-Setup/src/utils/asyncHandler.js
@@ -14,7 +14,7 @@
 
 // 2. Promise Method ->
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
