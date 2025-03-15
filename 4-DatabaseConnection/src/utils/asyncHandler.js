f25147818cef5a1dcbfc32e4f0e78043faedcc95
ShamsadAlam <nobleperson7571@gmail.com>
2025-03-15 08:54:07 +0530

    ApiError and ApiResponse Wrapper updated

--- 4-DatabaseConnection/src/utils/asyncHandler.js
+++ 4-DatabaseConnection/src/utils/asyncHandler.js
@@ -1,24 +1,28 @@
-const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
-};
+// wrapper of asyncHandler used for all async functions
 
-export default asyncHandler;
-// export {asyncHandler}
-
-// method of intialization of function into function
-// 1. const asyncHandler = () => {};
-// 2. const asyncHandler = (func) => {() => {}};
-// 3. const asyncHandler = (func) => async () => {};
-
+//  1. try-catch method ->
 // const asyncHandler = (func) => async (req, res, next) => {
 //     try {
 //       await func(req,res,next)
 //   } catch (error) {
 //     res.status(error.code || 500).json({
 //       success: false,
-//       messgae: err.message,
+//       messgae: error.message,
 //     });
 //   }
 // };
+
+// 2. Promise Method ->
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+// export default asyncHandler;
+
+// method of intialization of function into function
+// 1. const asyncHandler = () => {};
+// 2. const asyncHandler = (func) => {() => {}};
+// 3. const asyncHandler = (func) => async () => {};
