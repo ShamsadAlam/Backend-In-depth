01376fb982d0c24842a1820c8756fdb84f1a92dc
ShamsadAlam <nobleperson7571@gmail.com>
2025-03-15 08:34:27 +0530

    asyncHandler update done

--- 4-DatabaseConnection/src/utils/asyncHandler.js
+++ 4-DatabaseConnection/src/utils/asyncHandler.js
@@ -1,4 +1,24 @@
-const asyncHandler = () => {};
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 export default asyncHandler;
 // export {asyncHandler}
+
+// method of intialization of function into function
+// 1. const asyncHandler = () => {};
+// 2. const asyncHandler = (func) => {() => {}};
+// 3. const asyncHandler = (func) => async () => {};
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//       await func(req,res,next)
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       messgae: err.message,
+//     });
+//   }
+// };
