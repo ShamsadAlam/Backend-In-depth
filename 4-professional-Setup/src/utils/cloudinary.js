ddecb23ecf858af519dabfce98239339e893a7b1
ShamsadAlam <nobleperson7571@gmail.com>
2025-03-18 20:41:37 +0530

    fixed cloudinary issue

--- 4-professional-Setup/src/utils/cloudinary.js
+++ 4-professional-Setup/src/utils/cloudinary.js
@@ -5,49 +5,33 @@
 
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import { config } from "process";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Cloudinary configuration
-cloudinary.config({
+const configData = cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
+  console.log("local File Path:", localFilePath);
+
   try {
     if (!localFilePath) return null;
     //upload the file on cloudinary
     const uploadResult = await cloudinary.uploader
-      .upload(
-        "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-        {
-          public_id: "shoes",
-          resource_type: "auto",
-        }
-      )
+      .upload(localFilePath, {
+        resource_type: "auto",
+      })
       .catch((error) => {
         console.log("Error uploading on cloudinary.", error);
       });
     console.log("file is uploaded on cloudinary.", uploadResult);
+    fs.unlinkSync(localFilePath);
     return uploadResult;
-
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url("shoes", {
-      fetch_format: "auto",
-      quality: "auto",
-    });
-
-    console.log(optimizeUrl);
-
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url("shoes", {
-      crop: "auto",
-      gravity: "auto",
-      width: 500,
-      height: 500,
-    });
-
-    console.log(autoCropUrl);
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed.
     return null;
