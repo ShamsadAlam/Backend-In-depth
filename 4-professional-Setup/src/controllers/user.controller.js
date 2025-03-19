2f947ff07ef776e89587de372d28472e534a6e0b
ShamsadAlam <nobleperson7571@gmail.com>
2025-03-19 21:21:42 +0530

    minor bug fixes resolved

--- 4-professional-Setup/src/controllers/user.controller.js
+++ 4-professional-Setup/src/controllers/user.controller.js
@@ -122,9 +122,9 @@ const loginUser = asyncHandler(async (req, res) => {
     user._id
   );
 
-  const loggedInUser = await user
-    .findById(user._id)
-    .select("-password -refreshToken");
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
   const options = {
     httpOnly: true,
